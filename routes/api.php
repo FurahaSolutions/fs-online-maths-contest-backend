9726c821f67e249d2803b1052d13b0830ad9b43d
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-12-20 09:17:57 +0300

    Logged in user can be viewed

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login/social', 'App\\Http\\Controllers\\SocialUserLogin@login');
 
 Route::middleware(['auth:api'])->group(function () {
+    Route::get('user','App\\Http\\Controllers\\AuthController@index');
     Route::resource('contests', 'App\\Http\\Controllers\\ContestController');
     Route::resource('contest-editions', 'App\\Http\\Controllers\\ContestEditionController');
 });
